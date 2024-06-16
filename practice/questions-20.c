73c7ca5a9c0cbdafdfe69602a4aa20841c16885d
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-06-16 13:55:26 +0530

    fibbo

--- practice/questions-20.c
+++ practice/questions-20.c
@@ -1,7 +1,29 @@
-/* Write a c program to print the fibonacci series in a given range */
+/* Write a c program to print the fibbonacci series in a given range */
 #include <stdio.h>
 #include <math.h>
-int main(){
-    printf("");
-        return 0;
+int main()
+{
+    int t1 = 0, t2 = 1, next, i, n;
+    printf("enter range for fibbonaci series : ");
+    scanf("%d", &n);
+    printf("fibbonacci series\n");
+    for (i = 0; i <= n; i++)
+    {
+        if (i == 0)
+        {
+            printf("%d\n", t1);
         }
+        else if (i == 1)
+        {
+            printf("%d\n", t2);
+        }
+        else
+        {
+            next = t1 + t2;
+            printf("%d\n", next);
+            t1 = t2;
+            t2 = next;
+        }
+    }
+    return 0;
+}
