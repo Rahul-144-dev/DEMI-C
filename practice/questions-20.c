1929838920b02b7d77adfe0bab45b39ed4834573
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-06-24 08:09:25 +0530

    b

--- practice/questions-20.c
+++ practice/questions-20.c
@@ -3,26 +3,26 @@
 #include <math.h>
 int main()
 {
-    int t1 = 0, t2 = 1, next, i, n;
+    int a = 0, b = 1, n, next;
     printf("enter range for fibbonaci series : ");
     scanf("%d", &n);
     printf("fibbonacci series\n");
-    for (i = 0; i <= n; i++)
+    for (int i = 0; i <= n; i++)
     {
-        if (i == 0)
+        if (i == 0) /*1!=0*/
         {
-            printf("%d\n", t1);
+            printf("%d\n", a);
         }
-        else if (i == 1)
+        else if (i == 1) /*2!=0*/
         {
-            printf("%d\n", t2);
+            printf("%d\n", b);
         }
         else
         {
-            next = t1 + t2;
-            printf("%d\n", next);
-            t1 = t2;
-            t2 = next;
+            next = a + b;         /*(1+2=3)*/
+            printf("%d\n", next); /*next=3*/
+            a = b;                /*a=1=b*/
+            b = next;             /*b=2=next*/
         }
     }
     return 0;
---
+++ prime-diffrent-way.c
@@ -0,0 +1,21 @@
+/* prime number in diffrent way */
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n;
+    printf("enter number : ");
+    scanf("%d", &n);
+    for (int i = n; i <= n; i++)
+    {
+        if (n % 2 != 0 && n % (n - 1) != 0)
+        {
+            printf("prime");
+        }
+        else
+        {
+            printf("not-prime");
+        }
+    }
+    return 0;
+}
