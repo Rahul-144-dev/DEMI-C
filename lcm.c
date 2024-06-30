b004065751b95233891a54de4a1791f79521835f
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-06-30 15:10:18 +0530

    lcm

--- lcm.c
+++ lcm.c
@@ -8,9 +8,20 @@ int main()
     scanf("%d", &n1);
     printf("enter number : ");
     scanf("%d", &n2);
+    // if (n1 > n2)
+    //     max = n1;
+    // else
+    //     max = n2;
+    max = n1 > n2 ? n1 : n2;
     while (1)
     {
+        if ((max % n1 == 0) && (max % n2 == 0))
+        {
+            lcm = max;
+            printf("%d", lcm);
+            break;
+        }
+        ++max;
     }
-
     return 0;
 }
